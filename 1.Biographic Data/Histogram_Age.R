f641355d443bb7675b1a2a8a9f39ede55c7012c2
Karnatiswetcha <karnatiswetcha@gmail.com>
2018-12-16 23:35:51 -0600

    Adding

--- 1.Biographic Data/Histogram_Age.R
+++ 1.Biographic Data/Histogram_Age.R
@@ -3,4 +3,5 @@ data_hist<-read.csv("C:\\Users\\Swetcha\\Desktop\\Stats\\Project\\Methodist micr
 data_hist1<-data_hist[1:15, ]
 barplot(table(factor(data_hist1$Age)),xlab="Age in years",ylab="Frequency",col="cadetblue",main="Histogram for Age Distribution",font=2,family='sans',cex.main=1.8,cex.lab = 1.3)
 mean(data_hist$Age[1:15])
-
+##########How r u #################	
+########How do u do ##########
