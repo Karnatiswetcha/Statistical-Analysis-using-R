3ea1b9e0373fbafc662ea90411a19eccc8d56c36
Karnatiswetcha <karnatiswetcha@gmail.com>
2018-12-16 23:07:23 -0600

    Removing print

--- 1.Biographic Data/Histogram_Age.R
+++ 1.Biographic Data/Histogram_Age.R
@@ -2,7 +2,6 @@
 ####### Histogram for Age Distribution #####
 data_hist<-read.csv("C:\\Users\\Swetcha\\Desktop\\Stats\\Project\\Methodist microsurgery study\\MicrosurgeryPerformance1.csv",header = TRUE,sep=",")
 data_hist1<-data_hist[1:15, ]
-data_hist1
 barplot(table(factor(data_hist1$Age)),xlab="Age in years",ylab="Frequency",col="cadetblue",main="Histogram for Age Distribution",font=2,family='sans',cex.main=1.8,cex.lab = 1.3)
 mean(data_hist$Age[1:15])
 
